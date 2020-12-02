75be3300dd53f517b69da56b4f9fe96ef2e3ef6f
tassiag <tassigomes@gmail.com>
2020-12-02 10:53:23 +0000

    changes

--- main.js
+++ main.js
@@ -1,6 +1,59 @@
-$(document).ready(function () {
-    $('.the-carousel').slick({
-        setting-name: setting-value
-      });
-  });
+let sliderImages = document.querySelectorAll(".slide");
+let arrowLeft = document.querySelector("#arrow-left");
+let arrowRight = document.querySelector("#arrow-right");
+let current = 0;
 
+//clear all images
+function reset() {
+  for (let i = 0; i < sliderImages.length; i++) {
+    sliderImages[i].style.display = "none";
+  }
+}
+//init slider
+
+function startSlide() {
+  reset();
+  sliderImages[0].style.display = "block";
+}
+
+startSlide();
+
+// show previous
+
+function slideLeft() {
+  reset();
+  sliderImages[current - 1].style.display = "block";
+  current--;
+}
+
+//show next
+function slideRight() {
+  reset();
+  sliderImages[current + 1].style.display = "block";
+  current++;
+}
+
+// left arrow click
+arrowLeft.addEventListener("click", function () {
+  if (current === 0) {
+    current = sliderImages.length;
+  }
+  slideLeft();
+});
+
+// right arrow click
+arrowRight.addEventListener("click", function () {
+  if (current === sliderImages.length - 1) {
+    current = -1;
+  }
+  slideRight();
+});
+
+//// search
+$(document).ready(function () {
+  $(".fa-search").click(function () {
+    $(".search, .input").toggleClass("active");
+    $("input[type='text']").focus();
+  });
+});
+////
